2c8ec6c8777565bdf5f03f2546d4f06ced7df78c
Damian Zaremba <damian@damianzaremba.co.uk>
2011-02-07 15:20:06 +0000

    Standardizing whitespace

--- elevator.c
+++ elevator.c
@@ -18,46 +18,46 @@ int is_executable(const char *file_path) {
         #ifdef DEBUG
             perror("Could not stat file\n");
         #endif
-		return 0;
-	}
+        return 0;
+    }
 
     if (!S_ISREG(stat_data.st_mode)) {
         #ifdef DEBUG
-			perror("Not a regular file\n");
-		#endif
+            perror("Not a regular file\n");
+        #endif
         return 0;
     }
 
     if (stat_data.st_uid == geteuid()) {
-			if(stat_data.st_mode & S_IXUSR){
-				return 1;
-			}
+            if(stat_data.st_mode & S_IXUSR){
+                return 1;
+            }
 
-			#ifdef DEBUG
-				perror("Owner but have no execute permissions\n");
-			#endif
-			return 0;
-	}
+            #ifdef DEBUG
+                perror("Owner but have no execute permissions\n");
+            #endif
+            return 0;
+    }
 
     if (stat_data.st_gid == getegid()){
-			if(stat_data.st_mode & S_IXGRP){
-				return 1;		
-			}
+            if(stat_data.st_mode & S_IXGRP){
+                return 1;        
+            }
 
-			#ifdef DEBUG
-				perror("Group but no execute permission\n");
-			#endif
-			return 0;
-	}
+            #ifdef DEBUG
+                perror("Group but no execute permission\n");
+            #endif
+            return 0;
+    }
 
-	if(stat_data.st_mode & S_IXOTH) {
-		return 1;
-	}
+    if(stat_data.st_mode & S_IXOTH) {
+        return 1;
+    }
 
-	#ifdef DEBUG
-		perror("No global execute permissions\n");
-	#endif
-	return 0;
+    #ifdef DEBUG
+        perror("No global execute permissions\n");
+    #endif
+    return 0;
 }
 
 
@@ -65,48 +65,48 @@ int is_executable(const char *file_path) {
 int main(int argc, char **argv) {
 
     if (getuid()  != allow_uid || getgid()  != allow_gid) {
-		#ifdef DEBUG
-	        perror("Failed uid check; aborting\n");
-		#endif
+        #ifdef DEBUG
+            perror("Failed uid check; aborting\n");
+        #endif
         return E_USERCREDENTIALVALIDATIONFAILURE;
     }
 
     if (argc < 2) {
-		#ifdef DEBUG
-	        perror("Too few arguments supplied; aborting\n");
-		#endif
-		return E_TOOFEWARGUMENTSSUPPLIED;
+        #ifdef DEBUG
+            perror("Too few arguments supplied; aborting\n");
+        #endif
+        return E_TOOFEWARGUMENTSSUPPLIED;
     }
 
     if(setuid(target_uid) != 0) {
-		#ifdef DEBUG
-			perror("SetUID failed\n");
-		#endif
-		return E_SETUIDFAILED;
-	}
-	
+        #ifdef DEBUG
+            perror("SetUID failed\n");
+        #endif
+        return E_SETUIDFAILED;
+    }
+    
     if(setgid(target_gid) != 0){
-		#ifdef DEBUG
-			perror("SetGID failed\n");
-		#endif
-		return E_SETGUIDFAILED;
-	}
+        #ifdef DEBUG
+            perror("SetGID failed\n");
+        #endif
+        return E_SETGUIDFAILED;
+    }
 
-	/* Increment argv to remove this binary from the call */
+    /* Increment argv to remove this binary from the call */
     argv++;
 
-	/* Check this after setuid in cause permissions dictate that the initial user cannot see it */
-	if(!is_executable(argv[0])) {
-		printf("%s\n", argv[0]);
-		#ifdef DEBUG
-			perror("File requested is not executable\n");
-		#endif
-		return E_INVALIDEXECUTABLE;
-	}
+    /* Check this after setuid in cause permissions dictate that the initial user cannot see it */
+    if(!is_executable(argv[0])) {
+        printf("%s\n", argv[0]);
+        #ifdef DEBUG
+            perror("File requested is not executable\n");
+        #endif
+        return E_INVALIDEXECUTABLE;
+    }
 
     execv(argv[0], argv);
-	#ifdef DEBUG
-		perror("Execution failed\n");
-	#endif
-	return E_EXECUTIONERROR;
+    #ifdef DEBUG
+        perror("Execution failed\n");
+    #endif
+    return E_EXECUTIONERROR;
 }
